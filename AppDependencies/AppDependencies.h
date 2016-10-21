9eb20139815453bc6cfb29786ce8084cd2b8638c
Mate Gujgiczer <mate.gujgiczer@icloud.com>
2016-10-22 00:59:34 +0200

    Initial Commit

---
+++ AppDependencies/AppDependencies.h
@@ -0,0 +1,19 @@
+//
+//  AppDependencies.h
+//  AppDependencies
+//
+//  Created by Gujgiczer Máté on 2016. 10. 22..
+//  Copyright © 2016. gujci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppDependencies.
+FOUNDATION_EXPORT double AppDependenciesVersionNumber;
+
+//! Project version string for AppDependencies.
+FOUNDATION_EXPORT const unsigned char AppDependenciesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppDependencies/PublicHeader.h>
+
+
